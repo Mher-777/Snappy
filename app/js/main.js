f9a058e80607a546b216b17fcfcf9d78424acd8e
Mher-777 <mherjan2003@mail.ru>
2020-01-10 17:34:36 +0400

    added responsive and edn project

--- app/js/main.js
+++ app/js/main.js
@@ -8,28 +8,46 @@ $(function () {
         nextArrow: '<button class="animated fadeInDownBig arrows arrows-left"></button>',
         autoplay: true,
         infinite: true,
+        responsive: [
+            {
+                breakpoint: 920,
+                settings: {
+                    arrows: false,
+                }
+            },
+        ]
     });
 
+
     $(window).on('scroll', function () {
-        if ($(window).scrollTop()) {
+        var scroll = $(window).scrollTop();
+        if (scroll >= 620) {
             $('.header').addClass('black').animate();
+            timer = 2000
+
         }
         else {
             $('.header').removeClass('black');
         }
     });
-
-    $('.fact__item-number').each(function () {
-        $(this).prop('Counter', 0).animate({
-            Counter: $(this).text()
-        }, {
-            duration: 5000,
-            easing: 'swing',
-            step: function (now) {
-                $(this).text(Math.ceil(now));
-            }
-        });
+    $(window).on('scroll', function () {
+        var scroll = $(window).scrollTop();
+        if (scroll >= 1000) {
+            $('.skill__info-line-result').addClass('--on')
+        }
     });
-
+    let timer = setTimeout(function MySetInterval() {
+        $('.fact__item-number').each(function () {
+            $(this).prop('Counter', 0).animate({
+                Counter: $(this).text()
+            }, {
+                duration: 4000,
+                easing: 'swing',
+                step: function (now) {
+                    $(this).text(Math.ceil(now));
+                }
+            });
+        });
+    }, 2000);
     new WOW().init();
 })
